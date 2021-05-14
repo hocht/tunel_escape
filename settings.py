4c9180500ef72f56c24715121a34f57ff6d1802e
hocht <rodrigoestillao@gmail.com>
2021-05-14 18:31:56 -0500

    Creado modulo de settings

--- settings.py
+++ settings.py
@@ -1,9 +1,10 @@
 import pygame
+
 class Settings:
     """A class to store all settings for Tunnel escape"""
-    def__init__(self):
-    """initialize the game's settings"""
-    #Screen settings
-        self.screen_width = 800
-        self.screen_height = 600
+    def __init__(self):
+        """A class to store all settings for Tunnel escape"""
+        #Screen settings
+        self.screen_width = 400
+        self.screen_height = 400
         self.bg_color = (230,230,230)
--- tunelScape.py
+++ tunelScape.py
@@ -1,17 +1,21 @@
 import sys
-
 import pygame
 
+from settings import Settings
 class TunnelEscape:
     """Overall class to manage game assets and behaivor"""
     def __init__(self):
         """ initialize the game, and create game resources"""
         pygame.init()
-        self.screen = pygame.display.set_mode((800,600))
-        pygame.display.set_caption("Tunnel escape")
-        #set the background color
-        self.bg_color = (230,230,230)
-        
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width,
+            self.settings.screen_height))
+        pygame.display.set_caption("Tunnel escape") 
+
+
+
         def run_game(self):
             """start the main loop for the game"""
             while True:
@@ -20,7 +24,7 @@ class TunnelEscape:
                     if event.type == pygame.QUIT:
                         sys.exit()
                 #make the most recently drawn screen visible.
-                self.screen.fill(self.bg_color)
+                self.screen.fill(self.settings.bg_color)
                 # make the most recently drawn screen visible
                 pygame.display.flip()
 if __name__ == '__main__':
