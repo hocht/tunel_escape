6ed3e87be8d95b79e6d3987d4c6e7c39ed3074d3
hocht <rodrigoestillao@gmail.com>
2021-05-14 15:06:11 -0500

    color de fondo agregado

---
+++ settings.py
@@ -0,0 +1,9 @@
+import pygame
+class Settings:
+    """A class to store all settings for Tunnel escape"""
+    def__init__(self):
+    """initialize the game's settings"""
+    #Screen settings
+        self.screen_width = 800
+        self.screen_height = 600
+        self.bg_color = (230,230,230)
--- tunelScape.py
+++ tunelScape.py
@@ -7,17 +7,20 @@ class TunnelEscape:
     def __init__(self):
         """ initialize the game, and create game resources"""
         pygame.init()
-
-        self.screen = pygame.display.set_mode((1200,800))
+        self.screen = pygame.display.set_mode((800,600))
         pygame.display.set_caption("Tunnel escape")
+        #set the background color
+        self.bg_color = (230,230,230)
         
         def run_game(self):
             """start the main loop for the game"""
-            while true:
+            while True:
                 #watch for keyboard and mouse events
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
                         sys.exit()
+                #make the most recently drawn screen visible.
+                self.screen.fill(self.bg_color)
                 # make the most recently drawn screen visible
                 pygame.display.flip()
 if __name__ == '__main__':
