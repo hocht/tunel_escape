fc09442a0f54b43682dd8867705b2f437ec4a076
hocht <rodrigoestillao@gmail.com>
2021-05-17 16:54:17 -0500

    Agregado modulo de nave

--- tunnelEscape.py
+++ tunnelEscape.py
@@ -2,8 +2,11 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
+
 class TunnelEscape:
     """Overall class to manage game assets and behaivor"""
+    
     def __init__(self):
         """ initialize the game, and create game resources"""
         pygame.init()
@@ -14,24 +17,25 @@ class TunnelEscape:
             self.settings.screen_height))
         pygame.display.set_caption("Tunnel escape") 
 
+        self.ship = Ship(self)
 
+    def run_game(self):
+        """start the main loop for the game"""
+        while True:
+            #watch for keyboard and mouse events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            #make the most recently drawn screen visible.
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
-        def run_game(self):
-            """start the main loop for the game"""
-            while True:
-                #watch for keyboard and mouse events
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        sys.exit()
-                #make the most recently drawn screen visible.
-                self.screen.fill(self.settings.bg_color)
-                # make the most recently drawn screen visible
+            # make the most recently drawn screen visible
             pygame.display.flip()
 if __name__ == '__main__':
     # make a game instance and run the game
     ai = TunnelEscape()
     ai.run_game()
-
             
             
 
