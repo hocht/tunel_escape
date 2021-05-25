116ab47ba591f1d9e9cc9c27e82eb469d5eb5c5a
hocht <rodrigoestillao@gmail.com>
2021-05-25 17:32:41 -0500

    Cambios menores

--- scoreboard.py
+++ scoreboard.py
@@ -33,7 +33,7 @@ class Scoreboard:
         
         # Display the score at the top right of the screen
         self.score_rect = self.score_image.get_rect()
-        self.score_rect.right = self.screen_rect.right -32
+        self.score_rect.right = self.screen_rect.right -20
         self.score_rect.top = 20
     
     def show_score(self):
@@ -72,7 +72,7 @@ class Scoreboard:
         self.level_rect = self.level_image.get_rect()
         self.level_rect.right = self.score_rect.right
         self.level_rect.top = self.score_rect.bottom = + 32
-        
+
     def prep_ships(self):
         "Show how many ships are left."
         self.ships = Group()
